30bdc4d5b293ca00528996c0eac68ce952cb6a1a
ikazzzzz <ikazzz001@gmail.com>
2024-01-21 15:12:35 +0700

    Update dbConfig.ts

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import { edgeServerAppPaths } from "next/dist/build/webpack/plugins/pages-manifest-plugin"
 
 dotenv.config();
-const url = process.env.MONGO_URI as string 
+const url = process.env.MONGODB_URI as string 
 let connection: typeof mongoose
 
 const startDb = async () => {
